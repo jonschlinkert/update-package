6758d97a1cc1572dd4fde0b6f49f4fee348a5cb5
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-19 12:28:17 -0500

    build readme

---
+++ verbfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var verb = require('verb');
+var update = require('./');
+
+verb.engine('json', function json(str, options, cb) {
+  console.log(arguments)
+  if (typeof options === 'function') {
+    cb = options; options = {};
+  }
+  try {
+    cb(null, update(str));
+  } catch (err) { cb(err); }
+});
+
+
+verb.task('none', function() {
+  verb.src('package.json')
+    .pipe(verb.dest('.'));
+});
+
+verb.task('default', function() {
+  verb.src('.verb.md')
+    .pipe(verb.dest('.'));
+});
